589599eb507e7fe4647f78e0b2eff9ff12701174
mymeles <mymeles@ncsu.edu>
2023-01-12 16:11:53 -0500

    initial code

---
+++ emaple.py
@@ -0,0 +1,14 @@
+class emaple:
+    def __init__(self) -> None:
+        pass
+
+    def meles():
+        return 0
+
+    def paul():
+        return 0
+
+    def jack():
+        return 0
+    
+    
